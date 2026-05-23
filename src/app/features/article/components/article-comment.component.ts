c44cb4cbf6c4eabc5bc639860c17e9f800f60dec
PhaneendraVenepalli <phaneendranaidu237@gmail.com>
2026-05-23 19:33:52 +0530

    removed comment from branch1

--- src/app/features/article/components/article-comment.component.ts
+++ src/app/features/article/components/article-comment.component.ts
@@ -44,7 +44,6 @@ export class ArticleCommentComponent {
   @Input() comment!: Comment;
   @Output() delete = new EventEmitter<boolean>();
 
-  // hello from branch1
   canModify$ = inject(UserService).currentUser.pipe(
     map((userData: User | null) => userData?.username === this.comment.author.username),
   );
